f8a39a024e7ee855ffc09d5bf36a373a8f6172a0
Gustavo Kureck Piccoli <gustavokpiccoli@gmail.com>
2022-12-13 23:03:19 -0300

    test

--- lib/question.dart
+++ lib/question.dart
@@ -1,13 +1,12 @@
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
+ 
 
 class Questao extends StatelessWidget {
   const Questao({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(child: Text('TESTE'),);
+    return Container(child: Text('Mensagem 1'),);
   }
 }
 
